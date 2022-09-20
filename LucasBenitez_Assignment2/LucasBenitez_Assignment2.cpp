20dad3c4d309d2ab7344a5d793ab8abf91d80a45
lbenitez2019 <113250418+lbenitez2019@users.noreply.github.com>
2022-09-19 21:59:38 -0400

    Added loop control and formatting.

--- LucasBenitez_Assignment2/LucasBenitez_Assignment2.cpp
+++ LucasBenitez_Assignment2/LucasBenitez_Assignment2.cpp
@@ -13,67 +13,68 @@ using namespace std;						// Using the C++ std library namespace
 
 int main()
 {
-	double P; // principle loan amount
-	double M; // total monthly payment including principle plus interest
-	double r; // monthly interest rate
-	double n; // length of the loan in months
+	char STRT = 'y';    // checks whether or not the user wants to continue or quit the program
+	double P;			// principle loan amount
+	double M;			// total monthly payment including principle plus interest
+	double r;			// monthly interest rate
+	double n;			// length of the loan in months
 
 	cout.setf(ios::fixed);    //	format output for currency
 	cout.setf(ios::showpoint);//	format output for currency
 	cout.precision(2);		  //	format output for currency
 
 
-	// print out gretting 
-
-	// TEMP COMMENTING OUT TO MAKE TESTING FASTER -- BEGIN
-	/*
-	cout << "enter loan amount (in thousands): "; // ask for loan amount
-	cin >> P;
-	P = P * 1000;	// convert to thousands
-
-	cout << "enter interest rate: ";	// ask for interest rate
-	cin >> r;
-	r = r / 100 / 12;	// convert from annual to monthly percentage rate
-
-	cout << "enter length of loan (in months): ";	// ask for lenght of loan in months
-	cin >> n;
-	*/
-
-	// Delete the below 3 lines when done testing
-	P = 20000.00;
-	r = 4.4 / 100.0 / 12.0;
-	n = 36.0;
-
-	// TEMP COMMENTING OUT TO MAKE TESTING FASTER -- END
-
-
-
-	M = P * (r * pow(1 + r, n)) / (pow(1 + r, n) - 1);
-	cout << "the value of M is: " << M << endl;
-
-	double mi;	      // monthly interest amount
-	double mp;		  // monthly principle amount
-	double rb = P;	  // remaining balance
-	int lc = 1;	      // loop counter
-
-	// Table header
-	cout << "-------------------------------------------------------------------------------------" << endl;
-	cout << "| Mth | Monthly payment | Interest payment | Priciple payment | Remaining balance   |" << endl;
-	cout << "-------------------------------------------------------------------------------------" << endl;
-	while (lc < n + 1)
+	while (STRT == 'y' || STRT == 'Y')
 	{
-		mi = r * rb;  // calculate monthly interest amount
-		mp = M - mi;  // calculate monthly priciple amount
-		rb = rb - mp; // calculate remaining balance 
 
-		//cout << std::setfill(' ') << std::setw(14);
+		cout << "enter loan amount (in thousands): "; // ask for loan amount
+		cin >> P;
+		P = P * 1000;	// convert to thousands
 
-		cout << "  " << lc << "       $" << M << "             $" << setfill(' ') << setw(5) << mi << "            $" << mp << "              $" << rb << endl;
+		cout << "enter interest rate: ";	// ask for interest rate
+		cin >> r;
+		r = r / 100 / 12;	// convert from annual to monthly percentage rate
 
-		lc++;
+		cout << "enter length of loan (in months): ";	// ask for lenght of loan in months
+		cin >> n;
+
+
+		// equation 
+		M = P * (r * pow(1 + r, n)) / (pow(1 + r, n) - 1);
+		cout << "the value of M is: " << M << endl;
+
+		double mi;	      // monthly interest amount
+		double mp;		  // monthly principle amount
+		double rb = P;	  // remaining balance
+		int lc = 1;	      // loop counter
+
+		// Table header
+		// field width shown below for formatting
+		//        12345 12345678901234567 123456789012345678 123456789012345678 123456789012345678901
+		cout << "-------------------------------------------------------------------------------------" << endl;
+		cout << "| Mth | Monthly payment | Interest payment | Priciple payment | Remaining balance   |" << endl;
+		cout << "-------------------------------------------------------------------------------------" << endl;
+		while (lc < n + 1)
+		{
+			mi = r * rb;  // calculate monthly interest amount
+			mp = M - mi;  // calculate monthly priciple amount
+			rb = rb - mp; // calculate remaining balance 
+
+			//cout << std::setfill(' ') << std::setw(14);
+			//cout << "  " << lc << "       $" << M << "             $" << setfill(' ') << setw(5) << mi << "            $" << mp << "              $" << rb << endl;
+			cout << setfill(' ') << setw(4) << lc;
+			cout << "      $" << setfill(' ') << setw(10) << M;
+			cout << "      $" << setfill(' ') << setw(10) << mi;
+			cout << "        $" << setfill(' ') << setw(10) << mp;
+			cout << "        $" << setfill(' ') << setw(10) << rb;
+			cout << endl;
+
+			lc++;
+		}
+
+		// Prompt user if they want to go again. 
+		cout << "Continue? (y/n) ";
+		cin >> STRT;
+		
 	}
-
-
-
-	return 0;
 }
